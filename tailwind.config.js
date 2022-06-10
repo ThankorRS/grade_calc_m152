190714595dd6623654619c4fc6e64583da0f71c2
Robin Sigrist <sigrist@simpit.ch>
2022-06-10 20:15:39 +0200

    tailwind config change for colors, changed nav with new classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,19 @@ module.exports = {
     extend: {
       colors: {
         custom: {
+          // "white text on dark gray background"
           "color-primary-text":"#32292f",
           "color-primary-background":"#d1e3dd",
+          // "background text on dark gray white"
           "color-primary-viceversa-text":"#d1e3dd",
           "color-primary-viceversa-background":"#32292f",
+          // "middle gray text on light blue background"
           "color-secondary-text":"#575366",
           "color-secondary-background":"#5762D5",
+          // "blue background text on middle gray background"
           "color-secondary-viceversa-text":"#5762D5",
           "color-secondary-viceversa-background":"#5762D5",
+          // "exception: light dark text on dark gray background"
           "color-exception-text":"#A94D4D",
           "color-exception-background":"#32292F",
         },
