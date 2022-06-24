2eebf5466addcb66240bcaac9e805f7daf0913c7
Robin Sigrist <sigrist@simpit.ch>
2022-06-24 12:24:18 +0200

    several changes

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -8,8 +8,8 @@ export default defineComponent({
     },
     data(){
       let input = ref(1);
-      const formError = ref(false);
-      return {data, input, formError};
+      let text = ref();
+      return {data, input, text};
     },
     methods: {
       avg: function(grades: any){
@@ -29,6 +29,25 @@ export default defineComponent({
             grade: grade
           });
         }
+      },
+      newSub: function(name: string, data: any){
+        data.push({
+          id: (data.length + 1).toString(),
+          name: name,
+          grades: []
+        })
+      },
+      avgOverall: function() {
+        let sum = 0;
+
+        data.forEach(element => {
+          if (element.grades.length > 0) {
+            const subSum: any = element.grades.reduce((sum: number, current: number) => sum + current.grade, 0);
+
+            sum = sum + (Math.round(subSum / element.grades.length * 100) / 100);
+          }
+        });
+        return (Math.round(sum / data.length * 100) / 100).toFixed(1);
       }
     }
 })
@@ -36,23 +55,32 @@ export default defineComponent({
 
 <template>
   <div class="mt-20 mb-20">
-    <div class="my-2 flex items-center p-2">
-        <input v-model="input" type="text" class="w-16 block input" />
-        <button type="button" class="icon-btn ml-2 p-2 pl-3 pr-3 hover:bg-green-300" @click="add(input)">+</button>
+    <div class="my-2 flex items-center p-2 bg-slate-300">
+        <input v-model="text" type="text" placeholder="Fachname" class="block input sm:w-80 w-28 p-2 outline-0" @keyup.enter="newSub(text, data)"/>
+        <button type="button" class="icon-btn mx-2 hover:bg-green-300 p-2 pr-4 pl-4 mr-5" @click="newSub(text, data)">+</button>
+      <div class="p-2 pl-10 flex relative right-3 bg-slate-400 rounded sm:fixed">
+        <p class="pr-4">Gesamt</p>
+        <p><span class="font-medium mr-1">Ø</span>{{ avgOverall()}}</p>
+      </div>
     </div>
     <div v-for="element of data">
-      <article class="bg-white rounded mt-5 p-4">
+      <article class="bg-slate-300 rounded mt-5 p-4">
         <div class="flex items-center">
-          <p><span class="font-medium mr-1">Ø</span>{{ avg(element.grades) }}</p>
+          <p>
+            <span class="font-medium mr-1">Ø</span>
+            <span v-if="element.grades.length">{{ avg(element.grades) }}</span>
+            <span v-else>0</span>
+          </p>
           <h2 class="font-medium p-2">{{ element.name }}</h2>
         </div>
         <section class="my-4 flex flex-wrap">
             <div class="p-2 pl-4 pr-4 mr-2 mb-2 bg-gray-200 hover:bg-red-300 cursor-pointer" v-for="grade of element.grades" @click="remove(element, grade)">
-              <p>{{grade.grade.toFixed(2)}}</p>
+              <p class="text-orange-800" v-if="grade.grade <= 3.999">{{grade.grade.toFixed(2)}}</p>
+              <p class="text-green-800" v-else>{{grade.grade.toFixed(2)}}</p>
             </div>
         </section>
         <div class="my-2 flex items-center">
-            <input v-model="input" type="number" placeholder="6.0" min="1.0" max="6.0" class="w-16 block input" />
+            <input v-model="input" type="number" placeholder="6.0" min="1.0" max="6.0" class="w-16 block input p-2 outline-0" @keyup.enter="add(element, input)"/>
             <button type="button" class="icon-btn ml-2 p-2 pl-3 pr-3 hover:bg-green-300" @click="add(element, input)">+</button>
         </div>
       </article>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -8,25 +8,11 @@ export default defineComponent({
     data(){
       return data;
     },
-    // computed: {
-    //   av
-    // },
-    methods: {
-      avg: function(){
-        let sum = 0;
-
-        data.forEach(element => {
-          const subSum: any = element.grades.reduce((sum: number, current: number) => sum + current.grade, 0);
-          sum = sum + (Math.round(subSum / element.grades.length * 100) / 100);
-        });
-        return (Math.round(sum / data.length * 100) / 100).toFixed(1);
-      }
-    }
 })
 </script>
 
 <template>
-  <nav class="fixed bg-custom-color-primary-viceversa-background text-custom-color-primary-viceversa-text w-full h-20 flex items-end">
+  <nav class="fixed bg-custom-color-primary-viceversa-background text-custom-color-primary-viceversa-text w-full h-20 flex items-end z-50">
     <div class="logo">
       <img class="p-2 w-20 h-20 rounded-full"
         src="https://i.pinimg.com/originals/49/a7/24/49a7247bc0f530e1d427f6dab3c32cf0.jpg" 
@@ -37,10 +23,6 @@ export default defineComponent({
         Noten App 
         <small class="text-sm">by Sunie™</small>
       </p>
-    </div>
-    <div class="p-2 pl-10 flex">
-      <p class="pr-4">Gesamt</p>
-      <p><span class="font-medium mr-1">Ø</span>{{ avg() }}</p>
     </div>
   </nav>
 </template>
